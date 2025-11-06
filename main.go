f0689bc64550e1c5885a16920cd0a7625d9f9791
Anish Mahajan <anishmahajan778@gmail.com>
2025-11-06 20:34:34 +0530

    SQS Lamnda Invoke

--- main.go
+++ main.go
@@ -2,56 +2,70 @@ package main
 
 import (
 	"context"
-	"log"
+	"encoding/json"
+	"fmt"
 	"os"
-	"videotranscoder/aws-services/s3Buckets"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/joho/godotenv"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
+type SQSClientAPI interface {
+	SendMessage(ctx context.Context,
+		params *sqs.SendMessageInput,
+		optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error)
+}
+
+var (
+	sqsClient SQSClientAPI
+	queueURL  string
+)
+
+func init() {
+	var ok bool
+	queueURL = os.Getenv("SQS_QUEUE_URL")
+	if !ok {
+		panic("SQS_QUEUE_URL environment variable not set")
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic(fmt.Sprintf("unable to load SDK config, %v", err))
+	}
+
+	sqsClient = sqs.NewFromConfig(cfg)
+}
+
+func HandleRequest(ctx context.Context, s3Event events.S3Event) (string, error) {
+	for _, record := range s3Event.Records {
+		fmt.Printf("Processing record for S3 object: %s/%s\n", record.S3.Bucket.Name, record.S3.Object.Key)
+
+		messageBody, err := json.Marshal(record)
+		if err != nil {
+			fmt.Printf("Error marshalling S3 record: %v\n", err)
+			continue
+		}
+
+		sendInput := &sqs.SendMessageInput{
+			MessageBody: aws.String(string(messageBody)),
+			QueueUrl:    aws.String(queueURL),
+		}
+
+		result, err := sqsClient.SendMessage(context.TODO(), sendInput)
+		if err != nil {
+			fmt.Printf("Error sending message for %s: %v\n", record.S3.Object.Key, err)
+			return "Failed to send message", err
+		}
+
+		fmt.Printf("Message sent successfully for %s! Message ID: %s\n", record.S3.Object.Key, *result.MessageId)
+	}
+
+	return "All records processed successfully", nil
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
-
-	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalf("Error loading default config: %s", err)
-	}
-
-	s3Client := s3.NewFromConfig(sdkConfig)
-	s3Uploader := manager.NewUploader(s3Client)
-	s3Downloader := manager.NewDownloader(s3Client)
-
-	buckets := s3Buckets.S3Buckets{
-		S3Client:     s3Client,
-		S3Uploader:   s3Uploader,
-		S3Downloader: s3Downloader,
-	}
-
-	bucketName := "cloud-computing-project-video-uploads"
-	objectKey := "hellos3.txt"
-	content := "Hello, S3"
-
-	// upload file to a particular bucket
-	outKey, err := buckets.S3Upload(context.TODO(), bucketName, objectKey, content)
-	if err != nil {
-		log.Fatalf("failed to upload file: %v", err)
-	}
-	log.Printf("Successfully Uploaded file. Output Key: %s", outKey)
-
-	// download data from a particular bucket
-	data, err := buckets.S3Download(context.TODO(), bucketName, objectKey)
-	if err != nil {
-		log.Fatalf("Failed to download file: %v", err)
-	}
-	err = os.WriteFile(objectKey, data, 0644)
-	if err != nil {
-		log.Fatalf("Failed to Write to file: %v", err)
-	}
-	log.Printf("Successfully Download file: %v", objectKey)
+	lambda.Start(HandleRequest)
 }
