a49bef4a9fbc4178dd66f7bcff7adba612627fc8
Anish Mahajan <anishmahajan778@gmail.com>
2025-11-08 20:34:13 +0530

    Basic Frontend Done

--- server/routes.ts
+++ server/routes.ts
@@ -3,13 +3,13 @@ import { createServer, type Server } from "http";
 import { storage } from "./storage";
 
 export async function registerRoutes(app: Express): Promise<Server> {
-  // put application routes here
-  // prefix all routes with /api
+    // prefix all routes with /api
+    // put application routes here
 
-  // use storage to perform CRUD operations on the storage interface
-  // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
+    // use storage to perform CRUD operations on the storage interface
+    // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
 
-  const httpServer = createServer(app);
+    const httpServer = createServer(app);
 
-  return httpServer;
+    return httpServer;
 }
