9ae46b0852b571be809b926dde3b3bf8058b480a
Harpuneet Singh <singhharpuneet508@gmail.com>
2025-11-19 00:28:34 +0530

    New Frontend

--- client/src/components/examples/VideoUpload.tsx
+++
@@ -1,8 +0,0 @@
-import { VideoUpload } from "../VideoUpload";
-import { useState } from "react";
-
-export default function VideoUploadExample() {
-  const [file, setFile] = useState<File | null>(null);
-
-  return <VideoUpload selectedFile={file} onFileSelect={setFile} />;
-}
